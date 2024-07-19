a4d432e658fb3f26cfd8bf3e0f6e2fd0ed3159d9
Meet Jain <meetj665@gmail.com>
2024-07-19 22:43:41 +0530

    check point 1 db connected to next app

---
+++ packages/db/src/index.ts
@@ -0,0 +1,9 @@
+// packages/db/src/index.ts
+import { PrismaClient } from '@prisma/client';
+
+const client = new PrismaClient();
+
+// Add any other helper functions or utilities here
+
+export { client };
+// Export any other utilities or types
