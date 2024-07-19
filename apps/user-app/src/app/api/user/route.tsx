e107720ca500c513951656b32e6055e99e2daf0a
Meet Jain <meetj665@gmail.com>
2024-07-19 12:46:57 +0530

    trying to make user app using next create but not worked

--- apps/user-app/src/app/api/user/route.tsx
+++ apps/user-app/src/app/api/user/route.tsx
@@ -1,10 +1,8 @@
 import { NextResponse } from "next/server"
 // import { PrismaClient } from "@repo/db/client";
+import { client} from "@repo/db/client";
 
 // const client = new PrismaClient();
-import { client } from "@repo/db/client";
-
-
 
 export const GET = async () => {
     await client.user.create({
--- packages/store/src/hooks/useBalance.ts
+++
@@ -1,9 +0,0 @@
-'use client';  // Add this directive at the top if it's a component
-
-import { useRecoilValue } from 'recoil';
-import { balanceAtom } from '../atoms/balance';
-
-export const useBalance = () => {
-  const value = useRecoilValue(balanceAtom);
-  return value;
-};
